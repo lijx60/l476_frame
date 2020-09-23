afc10abea40b095f2538642456b275c434aa9b3e
lijx60 <fjsmlijx@126.com>
2020-09-23 23:07:18 +0800

    frame test

---
+++ MDK-ARM/RTE/_l476_frame/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'l476_frame' 
+ * Target:  'l476_frame' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
